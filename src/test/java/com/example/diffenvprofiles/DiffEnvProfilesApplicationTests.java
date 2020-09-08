5323dde3ee77bd18397e189d05a74b50c8121384
Anand Sinha <52150749@HYDMAC52150749.local>
2020-09-08 15:57:50 +0530

    Different profiles configuration and properties setting for spring-boot environment

---
+++ src/test/java/com/example/diffenvprofiles/DiffEnvProfilesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.diffenvprofiles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiffEnvProfilesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
